4e7adf0071c8933e9b8bb9b0a83077e5e6b40ebe
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:07:13 +0100

    Subforum page; contains list of all threads within

---
+++ frontend/src/pages/SubforumPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <h1 class="title">{{ this.$route.params.subforum.title }}</h1>
+    </div>
+    <ThreadList />
+  </div>
+</template>
+
+
+<script>
+import ThreadList from '../components/ThreadList.vue';
+
+export default {
+  components: {
+    ThreadList
+  },
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+
+</style>
