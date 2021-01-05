ae64d4b71a9bf2bdae247aef23ae7a0ef0ea3d26
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:06:07 +0100

    a single thread item in the threadlist

---
+++ frontend/src/components/ThreadListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="thread-container" @click="goToThreadPage">
+    <h1>{{thread.title}}</h1>
+    <h4>by {{thread.user}}</h4>
+    <h4>published at: {{thread.published_time}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['thread'],
+  methods: {
+    goToThreadPage() {
+      this.$router.push({ name: 'ThreadPage', params: {thread: this.thread} });
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+.thread-container {
+  width: 80vw;
+  background-color: gray;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+
+
+</style>
