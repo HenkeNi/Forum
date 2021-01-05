d3d643f8a1c587d4c7ab90525330c9558161000c
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:07:22 +0100

    vue config file

---
+++ frontend/src/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer: {
+      proxy:
+      {
+        "^/rest/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
+        "^/api/":  { target: "http://localhost:3000", ws: true, changeOrigin: true },
+        "^/auth/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
+      }
+    }
+  }
