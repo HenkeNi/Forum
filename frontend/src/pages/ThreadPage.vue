29cff257ec973a479adca3aa267221713153faf4
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:08:23 +0100

    Page for a thread, will contain posts posted by users

---
+++ frontend/src/pages/ThreadPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{thread.id}}
+    {{thread.title}}
+  </div>
+</template>
+
+<script>
+export default {
+  //name: 'ThreadPage',
+  props: ['thread'],
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    fetchPosts() {
+      //let res = await fetch('');
+      console.log("FETCHING POSTS");
+    }
+  },
+  created() {
+    this.fetchPosts();
+  }
+
+}
+</script>
+
+
+  
+<style scoped>
+
+</style>
