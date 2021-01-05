994a0add6e60debe5cd4b0dd89d50c1eb3478293
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:09:48 +0100

    index

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const session = require('express-session');
 const store = require('better-express-store');
+const restRoutes = require('./routes/restRoutes');
+
 
 const app = express();
+
 const RestApi = require('./RestApi');
 const ACL = require('./ACL');
 const ACLsettings = require('./ACLsettings')
 
 
-
 // Make express able to read 'req.body'
 app.use(express.json()); 
 
+
+
+app.use((error, req, res, next) => {
+  if (error) {
+    res.status(500);
+    res.json({
+      error: 'An error occurred',
+      errorDetails: error
+    });
+  }
+})
 
 // Add express-session as middleware to our express app (to handle sessions)
 app.use(session({
@@ -19,7 +32,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: { secure: 'auto' },
-  store: store({ dbPath: './database.db' })
+  store: store({ dbPath: '../database/database.db' })
 }));
 
 // Add own middleware for handling ACL (Access Controll)
@@ -27,11 +40,14 @@ app.use(ACL(ACLsettings));
 
 
 // Create the REST api
-new RestApi(app, '/api/', './database.db');
+new RestApi(app, '/api/', '../database/database.db');
+//new RestApi(app, '/api/', './database.db');
 
 
 // Start web server
 app.listen(3000, () => { console.log('Listening on port 3000') }); 
+
+app.use('/rest', restRoutes);
 
 
 
