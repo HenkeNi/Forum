77886fb2945aa3d9948585aa284c071150261655
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:03:36 +0100

    Simple footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer">
+    <h4>@Batman</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      some: 1,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  /* height: 100px; */
+  background-color: yellow;
+  text-emphasis: center;
+  border-top: solid black 1px;
+  display: flex;
+  justify-content: center;
+}
+</style>
