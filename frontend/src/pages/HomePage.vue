529f6eb79a7ee90ab99321bb870ad55f97535116
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-05 23:06:20 +0100

    homepage that contains list of subforums

---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <subforum-list />
+  </div>
+</template>
+
+<script>
+import SubforumList from '../components/SubforumList.vue';
+
+export default {
+  components: {
+    SubforumList
+  }
+}
+</script>
+
+<style scoped>
+
+.home-page {
+  margin-top: 0px;
+  background-color: gray;
+}
+
+</style>
