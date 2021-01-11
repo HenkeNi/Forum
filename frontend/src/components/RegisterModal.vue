21e6c8146042c9ceab6002bb029b1f5b4f71892e
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-11 20:58:08 +0100

    modals for sign in and register

---
+++ frontend/src/components/RegisterModal.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="modal" v-show="value">
+    <div class="form">
+      <h2 class="title">Register</h2>
+      <form>
+        <div class="input-fields">
+          <input required type="email" id="reg-email" name="reg-email" placeholder="email" /><br/>
+          <input required type="text" id="reg-username" name="reg-username" placeholder="username" /><br/>
+          <input required type="password" id="reg-password" name="reg-password" placeholder="password"/><br/>  
+        </div>
+        <p v-show="failed">Registration failed!</p>
+        <input type="submit" @click="register" value="Register" />      
+        <button @click.prevent="close">Close</button>
+      </form>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Register-Modal',
+   props: {
+    value: {
+      required: true,
+    }
+  },
+  data() {
+    return {
+      failed: false,
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("input", !this.value);
+    },
+    async register(e) {
+      e.preventDefault();
+      
+      //let res = await fetch('http://localhost:3000/api/users', {
+      let res = await fetch('http://localhost:3000/rest/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json' 
+        },
+        body: 
+          JSON.stringify({ 
+             "email": document.getElementById("reg-email").value, 
+              "password": document.getElementById("reg-password").value,
+              "username": document.getElementById("reg-username").value,
+              "userRole": "member"
+          })
+      });
+      res = await res.json();
+      console.log(res);
+
+      if (!res) {
+        console.log("FAILED!");
+        this.failed = true;
+      } else {
+        this.$store.commit('setCurrentUser', res);
+        //this.$store.commit('setIsLoggedIn', true)
+        this.$emit("input", !this.value);
+
+        console.log("SUCCESS");
+      }
+
+      // if (!re) {
+      //   console.log("Creation failed!"); // write message ... failed to login!
+      // } else {
+      //   console.log("Creation successfull!\n", user);
+      //   //this.$store.commit('setIsLoggedIn', true)
+      //   //console.log(this.$store.getters.isLoggedIn);
+      //   this.$emit("input", !this.value);
+      // }
+    }
+  },
+}
+</script>
+
+<style scoped>
+.modal {
+  position: absolute;
+  width: 100%;
+  height: 120%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+}
+
+.title {
+  text-decoration: underline;
+}
+
+.form {
+  width: 40%;
+  height: 30%;
+  /* padding: 20px 50px 50px; */
+  background-color: rgb(186, 187, 183);
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-flow: column wrap;
+  text-align: center;
+}
+
+
+
+.input-fields {
+  padding-bottom: 30px;
+}
+
+.input-fields input {
+  width: 100%;
+}
+
+</style>
---
+++ frontend/src/components/SignInModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="modal" v-show="value">
+    <div class="form">
+      <h2 class="title">Sign In</h2>
+      <form>
+        <div class="input-fields">
+          <input required type="email" id="email" name="email" placeholder="email"/><br/>
+          <input required type="password" id="password" name="password" placeholder="password"/><br/>  
+        </div>
+        <input type="submit" @click="login" value="Login" />      
+        <button @click.prevent="close">Close</button>
+        <p class="warning" v-show="failed">Wrong email or password!</p>
+      </form>
+    </div>
+  </div>  
+</template>
+
+
+<script>
+export default {
+  name: 'SignIn-Modal',
+  props: {
+    value: {
+      required: true,
+    }
+  },
+  data() {
+    return {
+      failed: false,
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("input", !this.value);
+    },
+    async login(e) { // TODO: put in store??
+      e.preventDefault();
+
+      //let user = await fetch('http://localhost:3000/api/login', {
+      let user = await fetch('http://localhost:3000/rest/login', {
+
+       method: 'POST',
+        headers: {
+          'Content-Type': 'application/json' 
+        },
+        body: 
+          JSON.stringify({ "email": document.getElementById("email").value, "password": document.getElementById("password").value})
+      });
+      user = await user.json();
+      
+      if (!user) {
+        this.failed = true;
+      } else {
+        console.log("Successfully logged in with:\n", user);
+        this.clearFields();
+        this.$store.commit('setCurrentUser', user);
+        this.$emit("input", !this.value);
+      }
+    },
+    clearFields() {
+        document.getElementById("email").value = "";
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+
+.modal {
+  position: absolute;
+  width: 100%;
+  height: 120%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+}
+
+.title {
+  text-decoration: underline;
+}
+
+.form {
+  width: 40%;
+  height: 30%;
+  /* padding: 20px 50px 50px; */
+  background-color: rgb(186, 187, 183);
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-flow: column wrap;
+  text-align: center;
+}
+
+
+
+.input-fields {
+  padding-bottom: 30px;
+}
+
+.input-fields input {
+  width: 100%;
+}
+
+.warning {
+  color: red;
+}
+
+</style>
