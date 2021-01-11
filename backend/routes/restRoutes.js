3558f9fe29954ae5cccc5317c2791adb8b47ad85
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-11 20:56:21 +0100

    routes for posting new threads and posts

--- backend/routes/restRoutes.js
+++ backend/routes/restRoutes.js
@@ -6,6 +6,18 @@ const restController = require('../controllers/restController');
 router.get('/subforums', restController.getAllSubforums);
 router.get('/threads/:subforumId', restController.getAllSubforumThreads);
 router.get('/users/:id', restController.getUserById);
+router.get('/posts/:threadId', restController.getThreadPosts);
+
+
+// POST
+router.post("/threads", restController.createThread);
+router.post("/posts", restController.createPost);
+//router.post('/threads/:subforumId', restController.createThread);
+
+
+
+
+
 //router.get('/users', restController.getUsers);
 //router.get('/threads', restController.getAllThreads);
 //router.get('/thread/:threadId', restController.getThreadPosts)
