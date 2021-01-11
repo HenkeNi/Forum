7f79018536fdc578a83bb63ad841170f16ec46e0
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-11 20:59:04 +0100

    page for subforum [contains list of posts]

--- frontend/src/pages/SubforumPage.vue
+++ frontend/src/pages/SubforumPage.vue
@@ -2,6 +2,7 @@
   <div class="subforum-page">
     <div>
       <h1 class="title">{{ this.$route.params.subforum.title }}</h1>
+      <h2 class="create" @click="createNewThread">+ Create new thread</h2>
     </div>
     <ThreadList :subforum="this.$route.params.subforum" />
   </div>
@@ -13,8 +14,16 @@ import ThreadList from '../components/ThreadList.vue';
 
 export default {
   components: {
-    ThreadList
+    ThreadList,
   },
+
+  methods: {
+    // TODO: maybe put thus code in subfou page and instead do router push to ThreadPage and do ti there....
+    createNewThread() {
+      // if user is not logged in return....
+      this.$router.push({ name: 'threadCreation', params: {subforum: this.$route.params.subforum} });
+    }
+  }
 }
 </script>
 
@@ -30,5 +39,13 @@ export default {
   text-decoration: underline;
 }
 
+.new-thread {
+  background-color: greenyellow;
+  display: inline;
+}
+
+.create {
+  cursor: pointer;
+}
 
 </style>
