e75d6ae059ee2b0332b999ce2660dc1cffa55b1b
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-11 20:59:31 +0100

    page for thread

--- frontend/src/pages/ThreadPage.vue
+++ frontend/src/pages/ThreadPage.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    {{thread.id}}
-    {{thread.title}}
+  <div class="thread-page">
+    <PostList :thread=this.thread />
   </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue';
+
 export default {
   //name: 'ThreadPage',
+  components: {
+    PostList,
+  },
   props: ['thread'],
   data() {
     return {
@@ -16,7 +20,8 @@ export default {
   },
   methods: {
     fetchPosts() {
-      //let res = await fetch('');
+      
+      //let res = await fetch(''); // TODO: fetch posts for thread id
       console.log("FETCHING POSTS");
     }
   },
@@ -26,9 +31,11 @@ export default {
 
 }
 </script>
-
-
   
 <style scoped>
+
+.thread-page {
+    background-color: gray;
+}
 
 </style>
