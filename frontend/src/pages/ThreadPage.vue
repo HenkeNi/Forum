f3f54c794167a98a20abcf60110d8ce7ae671957
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-12 01:44:11 +0100

    moved fetch posts logic to ThreadPage

--- frontend/src/pages/ThreadPage.vue
+++ frontend/src/pages/ThreadPage.vue
@@ -1,41 +1,116 @@
 <template>
   <div class="thread-page">
-    <PostList :thread=this.thread />
+    <NewPostModal :threadId="this.thread.id" v-model="newpostModalOpen"/>
+    <h1 class="title">{{thread.title}}</h1>
+    <div class="block" v-if="showCloseThread">
+      <h3 @click="closeThread">Close Thread</h3>
+    </div>
+    <div class="closed" v-if="isClosed">
+      <h3>Thread is Closed!</h3>
+    </div>
+    <PostList :posts="this.posts" :thread="this.thread" />
+    <div v-if="!isClosed">
+      <h2 class="new-post" @click="newPost">+ Add new post</h2>
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue';
+import NewPostModal from '../components/NewPostModal.vue';
+
 
 export default {
-  //name: 'ThreadPage',
   components: {
     PostList,
+    NewPostModal,
   },
   props: ['thread'],
   data() {
     return {
       posts: [],
+      isClosed: false,
+      newpostModalOpen: false,
+    }
+  },
+  computed: {
+    showCloseThread() {
+      let user = this.$store.getters.currentUser;
+
+      if (user) { return (user.userRole === "moderator" || user.userRole === "admin") && this.isClosed === false; }
+      return false;
     }
   },
   methods: {
-    fetchPosts() {
-      
-      //let res = await fetch(''); // TODO: fetch posts for thread id
-      console.log("FETCHING POSTS");
-    }
+    async fetchPosts() {
+      let res = await fetch(`http://localhost:3000/rest/posts/${this.thread.id}`); // TODO: FIX!!!!
+      res = await res.json();
+      this.posts = res;
+    },
+    async closeThread() {
+      let res = await fetch(`http://localhost:3000/rest/closethread/${this.thread.id}`, {
+        method: 'PUT',
+       });
+      res = await res.json();
+      console.log(res);
+    },
+    newPost() {
+      if (this.isClosed) { return; }
+      this.newpostModalOpen = !this.newpostModalOpen;
+      this.fetchPosts();
+    },
   },
   created() {
     this.fetchPosts();
-  }
+    this.isClosed = this.thread.active === 0 ? true : false;
+  },
+  mounted() {
+    this.fetchPosts();
+  },
 
 }
 </script>
   
 <style scoped>
-
 .thread-page {
-    background-color: gray;
+  display: flex;
+  flex-direction: column;
+  background-color: gray;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.title {
+  margin: 0px;
+  padding: 30px 0px 30px 0px;
+  text-align: center;
+  text-decoration: underline;
+  width: 80vw;
+  border: 1px solid yellow;
+  background-color: rgb(105, 105, 105);
+}
+
+.block {
+  text-align: center;
+  text-decoration: underline;
+  width: 80vw;
+  border: 1px solid yellow;
+  background-color: rgb(240, 145, 145);
+  cursor: pointer;
+}
+
+.closed {
+  text-align: center;
+  text-decoration: underline;
+  width: 80vw;
+  border: 1px solid yellow;
+  background-color: rgb(240, 145, 145);
+  cursor: pointer;
+}
+
+.new-post {
+  padding-bottom: 100px;
+  cursor: pointer;
 }
 
 </style>
