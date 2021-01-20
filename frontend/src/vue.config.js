c01d29fbe7eb4339b53852613b6041b6ee0e17d9
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-20 08:53:18 +0100

    changed port

--- frontend/src/vue.config.js
+++ frontend/src/vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
     devServer: {
       proxy:
       {
-        "^/rest/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
-        "^/api/":  { target: "http://localhost:3000", ws: true, changeOrigin: true },
-        "^/auth/": { target: "http://localhost:3000", ws: true, changeOrigin: true },
+        "^/rest/": { target: "http://localhost:4000", ws: true, changeOrigin: true },
+        "^/api/":  { target: "http://localhost:4000", ws: true, changeOrigin: true },
+        "^/auth/": { target: "http://localhost:4000", ws: true, changeOrigin: true },
       }
     }
   }
