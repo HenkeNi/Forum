27683f9cd53e6228e09f77a5f50b38f6a9370ccc
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-30 17:14:46 +0100

    new component for editing posts

---
+++ frontend/src/components/EditPost.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <textarea class="textarea" id="edit-message" type="text" name="edit-message" placeholder="Enter new message..." cols="40" rows="5" required></textarea>
+  <div>
+    <button @click="updateMessage">Update</button>
+    <button @click="cancel">Cancel</button>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+ methods: {
+   async updateMessage() {
+     console.log("UPDATE");
+    
+
+   },
+   cancel() {
+     this.$parent.shouldEdit = false;
+   }
+
+ }, 
+}
+</script>
+
+<style scoped>
+
+</style>
