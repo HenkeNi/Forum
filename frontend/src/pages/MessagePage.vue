878a2e4a18a49e2b3306884c9f4ada86b0144129
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-31 20:45:54 +0100

    New page for user messages

---
+++ frontend/src/pages/MessagePage.vue
@@ -0,0 +1,16 @@
+<template>  
+  <div>
+    <p>Send a message to {{ user.username }}</p>
+  </div>  
+</template>
+
+<script>
+export default {
+  props: ['user'],
+  
+}
+</script>
+
+<style scoped>
+
+</style>
