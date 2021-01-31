a256b2e433f62c9d5e1c0b279e69e1a222242b18
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-31 20:45:06 +0100

    list of users

---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="user-list">
+    <UserListItem 
+      v-for="(user, index) in users"
+      :key="user.id"
+      :user="user"
+      :index="index"
+    />
+  </div>
+</template>
+
+<script>
+import UserListItem from './UserListItem.vue';
+
+export default {
+  components: {
+    UserListItem,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  methods: {
+    async fetchAllUsers() {
+      let res = await fetch(`/rest/v1/users/`); 
+      this.users = await res.json();
+    }
+  },
+  created() {
+    this.fetchAllUsers();
+  },
+  
+}
+</script>
+
+
+<style scoped>
+.user-list {
+  margin-bottom: 50px;
+}
+
+</style>
---
+++ frontend/src/components/UserListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div @click="goToUserProfile" class="container">
+      <h2>{{user.username}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+props: ['user', 'index'],  
+methods: {
+  goToUserProfile() {
+    console.log("GO TO PROFILE"); 
+    this.$router.push("/"); // TEMPORARY FIX!!
+    this.$router.push({ name: 'ProfilePage', params: {user: this.user } });
+  }
+},
+}
+</script>
+
+<style scoped>
+.container {
+  width: 60vw;
+  background-color: rgb(44, 56, 61);
+  cursor: pointer;
+  color: white;
+  border: 2px solid black;
+  text-align: center;
+  margin-bottom: 5px;
+}
+.container {
+
+}
+
+</style>
