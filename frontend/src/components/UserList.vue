617c0181f9a63d1b221a864554dd543430fe802a
Henrik Jangefelt <henke9105@hotmail.com>
2021-02-13 23:07:49 +0100

    sort list of users

--- frontend/src/components/UserList.vue
+++ frontend/src/components/UserList.vue
@@ -13,6 +13,7 @@
 import UserListItem from './UserListItem.vue';
 
 export default {
+  props: ['sort'],
   components: {
     UserListItem,
   },
@@ -30,6 +31,21 @@ export default {
   created() {
     this.fetchAllUsers();
   },
+  watch: {
+    sort() {
+      if (this.sort === "az") {
+      this.users.sort((a, b) => (a.username.toLowerCase() > b.username.toLowerCase()) ? 1 : -1); 
+      }
+      if (this.sort === "role") {
+        this.users.sort((a, b) => (a.userRole < b.userRole) ? 1 : -1);
+      }
+      // if (this.sort === "rapported") {
+      //   this.users.sort((a, b) => (a.isRapported < b.isRapported) ? 1 : -1);
+      // }
+
+    }
+  }
+  
   
 }
 </script>
